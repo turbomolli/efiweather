aad63f73432fdd4d07cd926d90fd45caaf517146
Markus Lehtonen <makkeli@gmail.com>
2019-06-28 00:41:04 +0300

    Geolocation and forecast

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,6 +17,7 @@ app.use(cors());
 
 const fetchWeather = async () => {
   const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}&`;
+  console.log(endpoint);
   const response = await fetch(endpoint);
 
   return response ? response.json() : {};
@@ -27,6 +28,23 @@ router.get('/api/weather', async ctx => {
 
   ctx.type = 'application/json; charset=utf-8';
   ctx.body = weatherData.weather ? weatherData.weather[0] : {};
+  // ctx.body = weatherData;
+});
+
+const fetchForecast = async (params) => {
+  const endpoint = `${mapURI}/forecast?lat=${params.lat}&lon=${params.lon}&appid=${appId}&`;
+  console.log(endpoint);
+  const response = await fetch(endpoint);
+
+  return response ? response.json() : {};
+};
+
+router.get('/api/forecast/:lat/:lon', async ctx => {
+  const forecastData = await fetchForecast(ctx.params);
+  console.log(ctx.params);
+
+  ctx.type = 'application/json; charset=utf-8';
+  ctx.body = forecastData.list;
 });
 
 app.use(router.routes());
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -14,26 +14,116 @@ const getWeatherFromApi = async () => {
   return {};
 };
 
+const getForecastFromApi = async (lat, lon) => {
+  try {
+    const response = await fetch(`${baseURL}/forecast/${lat}/${lon}`);
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+
+  return {};
+}
+
 class Weather extends React.Component {
   constructor(props) {
     super(props);
 
+    this.getGeoLoc = this.getGeoLoc.bind(this);
+
     this.state = {
       icon: '',
+      lon: '',
+      lat: '',
+      forecastArr: [],
+      isLoading: true,
     };
   }
 
   async componentWillMount() {
+    await this.getGeoLoc();
     const weather = await getWeatherFromApi();
+
+    /*
+    console.log(this.state.lon + ' ' + this.state.lat + ' ffdf');
+
+    if (this.state.lon !== '') {
+      const forecast = await getForecastFromApi(this.state.lat, this.state.lon);
+      this.setState({
+        forecastArr: forecast,
+      });
+    }
+    // console.log(forecast);
+    */
     this.setState({ icon: weather.icon.slice(0, -1) });
+    
   }
+
+  componentDidMount() {
+    
+  }
+
+
+  getGeoLoc() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const currentLongitude = JSON.stringify(Math.round(position.coords.longitude));
+        const currentLatitude = JSON.stringify(Math.round(position.coords.latitude));
+  
+        console.log(currentLatitude);
+        console.log(currentLongitude);
+
+        getForecastFromApi(currentLatitude, currentLongitude)
+        .then(res => this.setState(
+          {
+            forecastArr: res,
+            lon: currentLongitude,
+            lat: currentLatitude,
+            isLoading: false,
+          }))
+        .catch(err => console.log(err));
+      },
+      error => alert(error.message),
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000,
+      }
+   );
+  }
+
 
   render() {
     const { icon } = this.state;
 
+    const current = this.state.forecastArr.slice(0, 1).map((item, index) => (
+      <img key={item.dt} alt="forecast" src={`/img/${item.weather[0].icon.slice(0, -1)}.svg`} />
+    ));
+
+    const forecast = this.state.forecastArr.slice(1, 6).map((item, index) => (
+      <li key={item.dt}>
+        <img height="40" alt={item.dt_txt} src={`/img/${item.weather[0].icon.slice(0, -1)}.svg`} />
+      </li>
+    ));
+
     return (
       <div className="icon">
-        { icon && <img alt="icon" src={`/img/${icon}.svg`} /> }
+        { /* this.state.isLoading && <h3>Loading weather</h3> */ }
+        {!this.state.lon && <div><img alt="icon" src={`/img/${icon}.svg`} />
+          <p>Cannot get geolocation. Using default Helsinki, FI</p></div>
+        }
+        {!this.state.isLoading && (
+          <div>
+            <h2>Weather</h2>
+            <p>longitude: {this.state.lon}</p>
+            <p>latitude: {this.state.lat}</p>
+            {current}
+            <div>
+              <ul>{forecast}</ul>
+            </div>
+
+          </div>
+        )}
       </div>
     );
   }
