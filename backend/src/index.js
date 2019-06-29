e902ddecfeb148de2661a1c08463c6fd1f73a500
Markus Lehtonen <makkeli@gmail.com>
2019-06-29 17:22:33 +0300

    Removed apikey from public repo

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || 'a0ee7426f0c7db7c449da875b08d9a96';
+const appId = process.env.APPID || '';
 const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
 const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
 
